e3576054a2a3ce23cdc7201f81f7709a58316037
Rasmus256 <rasmus256@gmail.com>
2023-06-12 06:54:50 +0000

    adds counter

--- src/api/rest.py
+++ src/api/rest.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 import numpy as np
 import asyncio
-import datetime
+from datetime import date
 
 app = FastAPI()
 
 @app.get("/api/udafmainframe")
 async def get_reachable_nodes():
-    start = datetime.now()
-    end = datetime.date(2022, 4, 16)
+    start = date.today()
+    end = date(2040, 7, 24)
 
-    days = np.busday_count(start, end)
-    return days
+    days = int(np.busday_count(start, end))
+    return {'days' :days}
 
 @app.get("/healthz", status_code=204)
 def healthcheck():
