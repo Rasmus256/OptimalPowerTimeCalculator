45bdd6e6fdc47196810c997311829c342de59e61
Rasmus256 <rasmus256@gmail.com>
2024-09-07 14:56:10 +0000

    Initial commit to new service

--- src/api/rest.py
+++ src/api/rest.py
@@ -1,17 +1,34 @@
 from fastapi import FastAPI
-import numpy as np
 import asyncio
-from datetime import date
+import requests
+from datetime import datetime, timedelta
+import json
 
 app = FastAPI()
 
+class EnergyPrice:
+    def __init__(self, fromTs, price):
+        self.fromTs = datetime.fromtimestamp(fromTs)
+        self.toTs = self.fromTs + timedelta(hours=1) - timedelta(seconds=1)
+        self.price = price
+
+FuturePrices = [
+    EnergyPrice(1725069600, 10.0),
+    EnergyPrice(1725073200, 3.0),
+    EnergyPrice(1725076800, 4.0)
+    ]
+
 @app.get("/api/next-optimal-hour")
 async def get_days_until_out_of_mainframe():
-    start = date.today()
-    end = date(2040, 7, 24)
-
-    days = int(np.busday_count(start, end))
-    return {'days' :days}
+    global FuturePrices
+    url = 'https://api.energy-charts.info/price?bzn=DK1'
+    response = requests.get(url)
+    contents = json.loads(response.content)
+    ts = contents["unix_seconds"]
+    prices = contents["price"]
+    FuturePrices = [EnergyPrice(e,f) for e,f in zip(ts,prices)]
+    result = min(FuturePrices, key=lambda r:r['price'])
+    return {'price' :result}
 
 @app.get("/healthz", status_code=204)
 def healthcheck():
