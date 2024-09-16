38c4a7143e3e4a1f90b149d5550e284164b97707
Rasmus256 <rasmus256@gmail.com>
2024-09-16 10:40:37 +0200

    Update rest.py

--- src/api/rest.py
+++ src/api/rest.py
@@ -12,23 +12,12 @@ class EnergyPrice:
         self.toTs = self.fromTs + timedelta(hours=1) - timedelta(seconds=1)
         self.price = price
 
-FuturePrices = [
-    EnergyPrice(1725069600, 10.0),
-    EnergyPrice(1725073200, 3.0),
-    EnergyPrice(1725076800, 4.0)
-    ]
-
 @app.get("/api/next-optimal-hour")
 async def get_days_until_out_of_mainframe():
-    global FuturePrices
     url = 'https://api.energy-charts.info/price?bzn=DK1'
-    response = requests.get(url)
-    contents = json.loads(response.content)
-    ts = contents["unix_seconds"]
-    prices = contents["price"]
-    FuturePrices = [EnergyPrice(e,f) for e,f in zip(ts,prices)]
-    result = min(FuturePrices, key=lambda r:r['price'])
-    return {'price' :result}
+    contents = json.loads(requests.get(url))
+    FuturePrices = [EnergyPrice(e,f) for e,f in zip(contents["unix_seconds"],contents["price"])]
+    return {'price' :min(FuturePrices, key=lambda r:r['price'])}
 
 @app.get("/healthz", status_code=204)
 def healthcheck():
