2647a7eaea37603d479d6096fbefbafc877eeaad
Rasmus256 <rasmus256@gmail.com>
2024-09-27 22:34:52 +0200

    adds primitive cache

--- src/api/rest.py
+++ src/api/rest.py
@@ -9,6 +9,8 @@ today = date.today()
 
 app = FastAPI()
 
+cachedPrices = {}
+
 class EnergyPrice:
     def __init__(self, fromTs, toTs, price):
         self.fromTs = datetime.fromisoformat(fromTs)
@@ -17,16 +19,21 @@ class EnergyPrice:
 
 @app.get("/api/next-optimal-hour")
 async def get_days_until_out_of_mainframe(numHoursToForecast = 2):
+    global cachedPrices
     numHoursToForecast = int(numHoursToForecast)
     today = date.today()
     tomorrow = today + timedelta(days=1)    
+    if not today.strftime('%m/%d/%Y') in cachedPrices:
+      cachedPrices[today.strftime('%m/%d/%Y')]= getprices(today)
+    if not tomorrow.strftime('%m/%d/%Y') in cachedPrices:
+      cachedPrices[tomorrow.strftime('%m/%d/%Y')]= getprices(tomorrow)
 
-    todaysprices= getprices(today)
-    tomorrowsprices= getprices(tomorrow)
     FuturePrices = []
-    FuturePrices.extend(todaysprices)
-    FuturePrices.extend(tomorrowsprices)
-    
+    FuturePrices.extend(cachedPrices[today.strftime('%m/%d/%Y')])
+    FuturePrices.extend(cachedPrices[tomorrow.strftime('%m/%d/%Y')])
+    utc=pytz.UTC
+    FuturePrices = [e for e in FuturePrices if e.toTs >= utc.localize(datetime.now())]
+
     startTs = 0
     endTs = 0
     min_sum = float('inf')
@@ -46,9 +53,7 @@ def getprices(dateToFind):
     if(string_json.status_code == 200):
         contents = json.loads(string_json.content)
         possibleDates = [EnergyPrice(e['time_start'],e['time_end'],e['DKK_per_kWh']) for e in contents]
-        utc=pytz.UTC
-        print(possibleDates)
-        return [e for e in possibleDates if e.toTs >= utc.localize(datetime.now())]
+        return possibleDates
     return []
 
 @app.get("/healthz", status_code=204)
