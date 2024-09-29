74aec42b606dd389ed3f8fa87ffffac3e75169aa
Rasmus256 <rasmus256@gmail.com>
2024-09-29 13:53:29 +0200

    adds support for partial hours

--- src/api/rest.py
+++ src/api/rest.py
@@ -18,9 +18,12 @@ class EnergyPrice:
         self.price = price
 
 @app.get("/api/next-optimal-hour")
-async def get_days_until_out_of_mainframe(numHoursToForecast = 2):
+async def get_days_until_out_of_mainframe(numHoursToForecast = '2h35m'):
     global cachedPrices
-    numHoursToForecast = int(numHoursToForecast)
+    hoursString = numHoursToForecast.split('h')[0]
+    minuteString = numHoursToForecast.split('h')[1].split('m')[0]
+    numHoursInt = int(hoursString)
+    numMinutesInt = int(minuteString)
     today = date.today()
     tomorrow = today + timedelta(days=1)    
     if not today.strftime('%m/%d/%Y') in cachedPrices:
@@ -34,8 +37,12 @@ async def get_days_until_out_of_mainframe(numHoursToForecast = 2):
     utc=pytz.UTC
     FuturePrices = [e for e in FuturePrices if e.toTs >= utc.localize(datetime.now())]
 
-    startTs = 0
-    endTs = 0
+    hoursToForecastInclPartial = numHoursInt
+    if numMinutesInt > 0:
+        hoursToForecastInclPartial+=1
+    
+    startIdx = 0
+    endIdx = 0
     min_sum = float('inf')
     for i in range(len(FuturePrices)-(numHoursToForecast-1)):
         window_sum = 0
@@ -43,9 +50,36 @@ async def get_days_until_out_of_mainframe(numHoursToForecast = 2):
             window_sum += FuturePrices[i+j].price
         if window_sum < min_sum:
             min_sum = window_sum
-            startTs = FuturePrices[i].fromTs
-            endTs = FuturePrices[i+numHoursToForecast-1].toTs
-    return {'price' : {'fromTs': startTs, 'toTs': endTs, 'price': min_sum/numHoursToForecast}}
+            startTs = i
+            endTs = i+numHoursToForecast-1
+    #Were we asked to forecast a partial hour? If so, either attach this partial hour to the beginning or the end - depending on price.
+    price = 0
+    if numMinutesInt>0:
+        firstHourPrice = FuturePrices[startIdx].price
+        lastHourPrice = FuturePrices[endIdx].price
+        if firstHourPrice < lastHourPrice:
+            startTs = FuturePrices[startIdx].fromTs
+            endTs = FuturePrices[endIdx-1].toTs + timedelta(minutes=numMinutesInt)
+            print(f'looping from {startIdx} to {endIdx}')
+            for i in range(startIdx, endIdx-1):
+                print(i)
+                min_sum += FuturePrices[i].price * 60
+            min_sum += FuturePrices[endIdx-1].price*numMinutesInt
+            price = min_sum / (numHoursInt*60 + numMinutesInt)
+        else:
+            startTs = FuturePrices[startIdx].fromTs +  timedelta(minutes=60-numMinutesInt)
+            endTs = FuturePrices[endIdx].toTs
+            print(f'looping from {startIdx} to {endIdx}')
+            for i in range(startIdx+1, endIdx):
+                print(i)
+                min_sum += FuturePrices[i].price * 60
+            min_sum += FuturePrices[endIdx-1].price*(numMinutesInt)
+            price = min_sum / (numHoursInt*60 + numMinutesInt)
+    else:
+        startTs = FuturePrices[startIdx].fromTs
+        endTs = FuturePrices[endIdx].toTs
+        price = min_sum/numHoursToForecast
+    return {'price' : {'fromTs': startTs, 'toTs': endTs, 'price': price}}
 
 def getprices(dateToFind):
     url = f'https://www.elprisenligenu.dk/api/v1/prices/{dateToFind.year}/{dateToFind.month:02d}-{dateToFind.day:02d}_DK1.json'
