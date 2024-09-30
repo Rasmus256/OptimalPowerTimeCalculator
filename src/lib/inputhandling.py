8c863c2931cf3984f6aa9dc269bbaa2fccd7b965
Rasmus256 <rasmus256@gmail.com>
2024-09-30 07:39:55 +0200

    Delete src/lib directory

--- src/lib/inputhandling.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import lib.graph as graph
-
-Lines = [line.strip().split(',') for line in
-         open('./lib/edges.csv', 'r').readlines() if not line.strip()
-         in os.getenv("BLACKLIST", "").split(";")]
-G = graph.constructGraph(Lines)
